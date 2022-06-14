6cb8f3f83ebd65479d791a245a4a1c31bedc66a3
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-15 00:43:27 +0200

    Created function compone t that displays the indicated number  datand allows to switch pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+// data
+import JsonData from "./MOCK_DATA.json";
+
+// react-paginate
+import ReactPaginate from "react-paginate";
+
+// styles 
+import "./App.css";
 
 function App() {
+  const [datas, setDatas] = useState(JsonData);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const datasPerPage = 5;
+  const pagesVisited = pageNumber * datasPerPage;
+  const displayDatas = datas.slice(pagesVisited, pagesVisited + datasPerPage);
+  const pageCount = Math.ceil(datas.length / datasPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* display data */}
+        {displayDatas.map((data) => {
+          const color = data.color
+          return (
+            <div className='data' key={data.id} >
+              <ul key={data.id} style={{backgroundColor: color}}>
+                <li>{data.id}</li>
+                <li>{data.name}</li>
+                <li>{data.year}</li>
+              </ul>
+            </div>
+          );
+        })}
+
+      {/* paginate */}
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        pageCount={pageCount}
+        showPageCount={false}
+        nextRel={null}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 }
-
 export default App;
